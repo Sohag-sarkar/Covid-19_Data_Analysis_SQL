d2f1d6c49aa981d0b020f8ecdc07f5175d7abcdd
SOHAG SARKAR <101954145+Sohag-sarkar@users.noreply.github.com>
2022-09-26 03:35:19 +0530

    Update Covid_19.sql

--- Covid_19.sql
+++ Covid_19.sql
@@ -1,16 +1,15 @@
 -- Checking Data
-SELECT * FROM project.covid_deaths1;
-
-
-select continent, location, date, population, total_cases, new_cases, total_deaths, new_deaths
-from project.covid_deaths1
+SELECT * FROM project.covid_deaths1
 where continent is not null
-order by 2;
+order by 3 ;
+SELECT * FROM project.covid_vaccination1
+where continent is not null
+order by 3 ;
 
 -- Total cases vs Total Deaths
 select location, date, population, total_cases,total_deaths, (total_deaths/total_cases) *100 as DeathPercentage
 from project.covid_deaths1
-where continent is not null and location like 'Afghanistan'
+where continent is not null and location like '%Afg%'
 order by 1;
 
 -- Total Cases vs Population
@@ -27,15 +26,16 @@ group by location, population
 order by Total_death desc;
 
 -- Countries with highest death count per population
-select location, population,max(total_deaths) as Total_death, max((total_cases/population)*100) as Max_Infection_rate
+select location, population,max(total_deaths) as Total_death, max((total_deaths/population)*100) as Percentage_death_rate
 from project.covid_deaths1
-where continent is null
-group by continent
+where continent is not null
+group by location
 order by Total_death desc;
 
 -- Highest Death by Continent
-Select continent, max(cast(total_deaths as unsigned)) as Total_death_count
-Where continent is null
+Select continent , max(cast(total_deaths as unsigned)) as Total_death_count
+from project.covid_deaths1
+Where continent is not null
 Group by continent
 Order by Total_death_count desc;
 
@@ -51,26 +51,29 @@ order by 3;
 SELECT * FROM project.covid_vaccination1;
 
 -- Total Population vs Vaccination
-select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
+select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,
+sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated 
 from project.covid_deaths1 dea join project.covid_vaccination1 vac
 on dea.location= vac.location and dea.date= vac.date
 where dea.continent is not null and vac.people_vaccinated is not null
 order by dea.location;
 
--- Use CTE
-With Popsvsvac (continent, location, dates, Populations, New_vaccinations, People_vaccinated, Rolling_people_vaccinated) as
+-- Use CTE to calculate Rolling people vaccination
+With PopsvsVac (continent, location, dates, Populations, New_vaccinations, People_vaccinated, Rolling_people_vaccinated) as
 (
-select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
+select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,
+sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
 from project.covid_deaths1 dea join project.covid_vaccination1 vac
 on dea.location= vac.location and dea.date= vac.date
 where dea.continent is not null and vac.people_vaccinated is not null
 order by dea.location
 )
-select * from Popsvsvac;
+select * from PopsvsVac;
 
 
--- Temp table
+-- Temp table to calculate Vaccination of People
 Drop table if exists Percentpopulationvaccinated ;
+Use project;
 Create table Percentpopulationvaccinated 
 (
 continent varchar(250),
@@ -83,9 +86,10 @@ Rolling_people_vaccinated int
 );
 Insert into Percentpopulationvaccinated
 (
-select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
+select dea.continent,dea.location,str_to_date(dea.date), dea.population,vac.new_vaccinations,vac.people_vaccinated,
+sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
 from project.covid_deaths1 dea join project.covid_vaccination1 vac
-on dea.location= vac.location and dea.date= vac.date
+on dea.location= vac.location and str_to_date(dea.date)= str_to_date(vac.date)
 where dea.continent is not null and vac.people_vaccinated is not null
 order by dea.location
 );
@@ -94,7 +98,8 @@ Select *,(Rolling_People_vaccinated/Population) as Percentage_vaccination from P
 
 -- Creating view for store data for later visualisation
 Create view Percent_pops_vac 
-as select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
+as select dea.continent,dea.location,dea.date, dea.population,vac.new_vaccinations,vac.people_vaccinated,
+sum(new_vaccinations) over (partition by dea.location Order by dea.location) as Rolling_People_vaccinated
 from project.covid_deaths1 dea join project.covid_vaccination1 vac
 on dea.location= vac.location and dea.date= vac.date
 where dea.continent is not null
